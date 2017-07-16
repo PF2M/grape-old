12e901c2b5b126668589fc07cbad8ed3fe12e410
Lane Behee <PF2M@users.noreply.github.com>
2017-07-15 22:50:08 -0400

    Initial commit

---
+++ grp_portal-php/my/follow-rel-posts.php
@@ -0,0 +1,5 @@
+<?php
+require_once '../../grplib-php/init.php';
+
+// Implement this later
+
---
+++ grp_portal-php/my/played.php
@@ -0,0 +1,6 @@
+<?php
+require_once '../../grplib-php/init.php';
+
+// Implement this later
+header('Content-Type: application/json; charset=utf-8');
+print '{"success":1}'; 
